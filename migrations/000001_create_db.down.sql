56f3b0b1881532673e0f6b0bdb7f681b32ca61c9
aleksandrsokolov <timac11@bk.ru>
2026-01-13 01:18:30 +0300

    minor correction

--- migrations/000001_create_db.down.sql
+++ migrations/000001_create_db.down.sql
@@ -2,7 +2,7 @@ DROP TRIGGER IF EXISTS update_user_updated_at ON user;
 DROP TRIGGER IF EXISTS update_balance_updated_at ON balance;
 DROP TRIGGER IF EXISTS update_order_updated_at ON order;
 DROP TRIGGER IF EXISTS update_accrual_updated_at ON accrual;
-DROP TRIGGER IF EXISTS update_withdraw_updated_at ON withdraw;
+DROP TRIGGER IF EXISTS update_withdrawal_updated_at ON withdraw;
 
 DROP TABLE IF EXISTS withdrawal;
 DROP TABLE IF EXISTS accrual;
